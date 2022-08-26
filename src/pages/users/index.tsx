889b6ce73b7d456d392772f85fa2482ee0f05906
Diego Arthur <digo.arthur@hotmail.com>
2022-08-26 11:25:38 +0100

    Factories and seeds (MirageJS)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 
 import { Checkbox, Flex, SimpleGrid, Box, Text, theme, Heading, Button, Icon, Table, Thead, Tr, Th, Tbody, Td, useBreakpointValue } from "@chakra-ui/react"
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -10,6 +11,10 @@ export default function UserList() {
         base: false,
         lg: true,
     })
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/api/users').then(response=>response.json()).then(data=>console.log(data))
+    },[])
     return (
         <Box>
             <Header />
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,22 +1,47 @@
-import {createServer,Model} from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
+import faker from 'faker'
 
-
-type User ={
-    name:string,
-    email:string,
-    created_at:string;
+type User = {
+   name: string;
+   email: string;
+   created_at: string;
 }
 
-export function makeServer(){
-    const server = createServer({
-        models:{
-            user: Model.extend<Partial<User>>({})
-        },
-        routes(){
-            this.get('/users');
-            this.post('/users');
-        }
-    })
+export function makeServer() {
+   const server = createServer({
+      models: {
+         user: Model.extend<Partial<User>>({})
+      },
 
-    return server;
+      factories: {
+         user: Factory.extend({
+            name(i: number) {
+               return `Usuário ${i + 1}`
+            },
+            email() {
+               return faker.internet.email().toLowerCase();
+            },
+            createdAt() {
+               return faker.date.recent(10);
+            },
+         })
+      },
+
+      seeds(server) {
+         server.createList('user', 200);
+      },
+
+      routes() {
+         this.namespace = 'api';
+         this.timing = 750;
+
+         this.get('users');
+         this.post('users');
+
+         this.namespace = '';
+         this.passthrough();
+      }
+   })
+
+   return server;
 }
