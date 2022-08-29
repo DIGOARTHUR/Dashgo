e46717d77f9184960f6e42b65c5a2e5f5afb5e03
Diego Arthur <digo.arthur@hotmail.com>
2022-08-30 00:24:42 +0100

    Signaling data refetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,7 @@ import { useQuery } from 'react-query'
 
 export default function UserList() {
 
-    const { data, isLoading, error } = useQuery('users', async () => {
+    const { data, isLoading,isFetching, error } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
         const users = data.users.map(user => {
@@ -52,6 +52,7 @@ export default function UserList() {
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">
                             Usuários
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500"/>}
                         </Heading>
                         <Link href="/users/create" passHref>
 
