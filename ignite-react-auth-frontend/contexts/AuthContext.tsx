77ac74d70d0f08fc36b3b81bc8750a356eff7168
Diego Arthur <digo.arthur@hotmail.com>
2022-09-05 09:07:04 +0100

    Configuring axios client

--- ignite-react-auth-frontend/contexts/AuthContext.tsx
+++ ignite-react-auth-frontend/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
     email: string;
@@ -10,20 +11,31 @@ type AuthContextData = {
     isAuthenticated: boolean;
 }
 
-type AuthProviderProps={
-    children:ReactNode;
+type AuthProviderProps = {
+    children: ReactNode;
 }
 
 export const AuthContext = createContext({} as AuthContextData)
 
 
-export function AuthProvider({ children }:AuthProviderProps) {
+export function AuthProvider({ children }: AuthProviderProps) {
+
     const isAuthenticated = false;
+
     async function signIn({ email, password }: SignInCredentials) {
-        console.log({ email, password });
+        try {
+            const response = await api.post('sessions', {
+                email,
+                password
+            })
+
+            console.log(response.data);
+        } catch(err) {
+            console.log(err)
+        }
     }
     return (
-        <AuthContext.Provider value={{signIn,isAuthenticated}}>
+        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
             {children}
         </AuthContext.Provider>
     )
