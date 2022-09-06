e2d500592925516d973c824064a5895ed081dfbf
Diego Arthur <digo.arthur@hotmail.com>
2022-09-06 09:16:37 +0100

    Saving user data

--- ignite-react-auth-frontend/contexts/AuthContext.tsx
+++ ignite-react-auth-frontend/contexts/AuthContext.tsx
@@ -1,41 +1,63 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
+import  Router  from 'next/router'
 
 type SignInCredentials = {
     email: string;
     password: string;
 }
 
+type User = {
+    email: string;
+    permissions: string[];
+    roles: string[];
+}
+
+
 type AuthContextData = {
     signIn(credentials: SignInCredentials): Promise<void>;
+    user:User;
     isAuthenticated: boolean;
+    
 }
 
 type AuthProviderProps = {
     children: ReactNode;
 }
 
+
 export const AuthContext = createContext({} as AuthContextData)
 
 
 export function AuthProvider({ children }: AuthProviderProps) {
 
-    const isAuthenticated = false;
+    const [user, setUser] = useState<User>();
+   
+    const isAuthenticated = !!user;
 
     async function signIn({ email, password }: SignInCredentials) {
         try {
             const response = await api.post('sessions', {
                 email,
-                password
+                password,
             })
 
+            const {token,refreshToken, permissions, roles } = response.data
+
+            setUser({
+                email,
+                permissions,
+                roles,
+
+            })
+            Router.push('/dashboard')
             console.log(response.data);
-        } catch(err) {
+        } catch (err) {
             console.log(err)
         }
     }
     return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+        <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
             {children}
         </AuthContext.Provider>
     )
