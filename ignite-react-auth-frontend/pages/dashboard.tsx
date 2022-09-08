b7b039ef11bfb4f9fa9de8a5a45cdf438e9c201e
Diego Arthur <digo.arthur@hotmail.com>
2022-09-08 23:52:18 +0100

    Creating permission hook

--- ignite-react-auth-frontend/pages/dashboard.tsx
+++ ignite-react-auth-frontend/pages/dashboard.tsx
@@ -1,16 +1,23 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { api } from "../services/api"
 
 
-export default function Dashboard(){
-    const {user} = useContext(AuthContext)
-
-    useEffect(() => {
-        api.get('me').then(response=>console.log(response))
-        .catch(err => console.log(err))
+export default function Dashboard() {
+    const { user } = useContext(AuthContext)
+    const userCanSeeMetrics = useCan({
+        permissions: ['metrics.list']
     })
-    return(
-        <h1>Dashboard: {user?.email}</h1>
+    useEffect(() => {
+        api.get('me').then(response => console.log(response))
+            .catch(err => console.log(err))
+    })
+    return (
+        <>
+            <h1>Dashboard: {user?.email}</h1>
+            {userCanSeeMetrics && <div>Métricas</div>}
+        </>
+
     )
 }
